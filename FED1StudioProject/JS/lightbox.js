bc1df295c082941885455488434900e47d352217
1992cma <1992cma@gmail.com>
2021-08-05 20:42:05 -0500

    Created a lightbox and updated all of the other pages in the project

---
+++ FED1StudioProject/JS/lightbox.js
@@ -0,0 +1,23 @@
+﻿//**********Custom Lightbox**********
+//When the user clicks on a thumbnail image, display the lightbox container
+//and insert an <img> tag
+
+$(".thumb").on("click", function () {
+
+    //Find the src of the image that was clicked and store it as a variable
+    var imgSrc = $(this).attr("src");
+
+    //Plug an <img> tag into #lightbox-content - use imgSrc as the src
+    $("#lightbox-content").html('<img src="' + imgSrc + '" class="img-responsive" />');
+
+    //Make the lightbox visible by fading in
+    $("#lightbox-container").fadeIn(500);
+
+    //When the user clicks anywhere in the #lightbox-container, fadeOut()
+    $("#lightbox-container").on("click", function () {
+
+        $(this).fadeOut(500);
+
+    });//end fadeout
+
+});//end .thumb click
---
+++ FED1StudioProject/JS/validation.js
@@ -0,0 +1,87 @@
+﻿function validateForm(event) {
+    
+    //Gather all HTML controls into a collection instead of creating separate variables for each
+
+    let controls = document.getElementsByClassName('form-control');
+    console.log(controls);
+
+    let validationMessages = document.getElementsByClassName('invalid');
+    console.log(validationMessages);
+    
+    //Regular expression object for email
+    let rxpEmail = new RegExp(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
+    
+    //Regular expression object for Name
+    let rxpName = new RegExp(/^[A-Z]+$/i);
+
+    //Check the length of ALL controls - if any have NOT been filled out by the user, stop the form
+    //from submitting at all. 
+
+    if (controls['name'].value.length == 0 || controls['email'].value.length == 0 || 
+        controls['subject'].value.length == 0 || controls['message'].value.length == 0 ||
+        !rxpEmail.test(controls['email'].value) || !rxpName.test(controls['name'].value)) {
+    
+        //stop form from submitting
+        event.preventDefault();
+
+        //check individual controls and display error message if needed
+
+        //---------------Name Validation---------------
+        
+        //Check the length
+        if (controls['name'].value.length == 0) {
+            validationMessages['rfvName'].textContent = "* Name is required";
+        }
+        else {
+            validationMessages['rfvName'].textContent = "";
+        }
+        
+        //Regex
+        if (!rxpName.test(controls['name'].value) && controls['name'].value.length > 0) {
+            validationMessages['rfvName'].textContent = "* Name must only include alphabetical characters";
+        }
+
+        if (rxpName.test(controls['name'].value) && controls['name'].value.length > 0) {
+            validationMessages['rfvName'].textContent = "";
+        }
+
+        //---------------Email Validation---------------
+        //length
+        if (controls['email'].value.length == 0) {
+            validationMessages['rfvEmail'].textContent = "* Email is required";
+        }
+        else {
+            validationMessages['rfvEmail'].textContent = "";
+        }
+        //regex
+        if (!rxpEmail.test(controls['email'].value) && controls['email'].value.length > 0) {
+            validationMessages['rfvEmail'].textContent = "* Please enter a valid email address.";
+        }
+
+        if (rxpEmail.test(controls['email'].value) && controls['email'].value.length > 0) {
+            validationMessages['rfvEmail'].textContent = "";
+        }
+        
+
+        //---------------Subject Validation---------------
+
+        if (controls['subject'].value.length == 0) {
+            validationMessages['rfvSubject'].textContent = "* Subject is required";
+        }
+        else {
+            validationMessages['rfvSubject'].textContent = "";
+        }
+
+
+        //---------------Message Validation---------------
+
+        if (controls['message'].value.length == 0) {
+            validationMessages['rfvMessage'].textContent = "* Message is required";
+        }
+        else {
+            validationMessages['rfvMessage'].textContent = "";
+        }
+
+    }
+
+}
